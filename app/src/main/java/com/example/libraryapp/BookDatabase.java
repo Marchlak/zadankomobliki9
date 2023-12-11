2a48d6b362a5e7d16af4e086309b745469f99935
marchlak <bingor1@wp.pl>
2023-12-11 16:05:24 +0100

    Zadanko 8 zrobione przejscie do 9

---
+++ app/src/main/java/com/example/libraryapp/BookDatabase.java
@@ -0,0 +1,47 @@
+package com.example.libraryapp;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Book.class}, version = 1, exportSchema = false)
+public abstract class BookDatabase extends RoomDatabase {
+    private static BookDatabase databaseInstance;
+    static final ExecutorService databaseWriteExecutor = Executors.newSingleThreadExecutor();
+
+    public abstract BookDAO bookDAO();
+
+    static BookDatabase getDatabase(final Context context) {
+        if(databaseInstance == null) {
+            databaseInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            BookDatabase.class, "book_database")
+                    .addCallback(roomDatabaseCallback)
+                    .build();
+        }
+        return databaseInstance;
+    }
+
+    private static final RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            databaseWriteExecutor.execute(() -> {
+                BookDAO dao = databaseInstance.bookDAO();
+                Book book = new Book("Book 0", "Book 0");
+                dao.insert(book);
+                book = new Book("Book 1", "Author 1");
+                dao.insert(book);
+                book = new Book("Book 2", "Author 2");
+                dao.insert(book);
+                book = new Book("Book 3", "Author 3");
+                dao.insert(book);
+            });
+        }
+    };
+}
